6da5bc0bfc45509d425c90da4f46bca15d92ec17
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-08 16:19:46 -0700

    4-puts.c further Betty correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 /*
- *	main - executed first and is the starting point
- *
- *	Should return 0
+ *	main - executed first and displays output using puts()
+ *	Return: 0
  */
 
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
