1246c3ac18f9722b180eda790ba463aeb8eb00fb
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-08 16:32:04 -0700

    5-printf.c commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - prints output using printf function
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	int printf("wit proper grammar, but the outcome is a piece of art,")
+		return (0);
+}
