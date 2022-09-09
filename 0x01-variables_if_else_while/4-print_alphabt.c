13370f01e1660f7843e3d5ecf33ce7a018b6fd24
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-09 10:50:46 -0700

    4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase excluding q & e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(c);
+		}
+		return (0);
+	}
+}
