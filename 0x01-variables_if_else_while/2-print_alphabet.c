4c22036690ba3ad5b14370856f2343df551a877a
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-09 08:03:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print out alphabet in lowercase, followed by new line
+ *
+ * Return (0)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c > 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
