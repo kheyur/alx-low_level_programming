0b14f6b8f904d7fc06edab2b69b8c5ac2c45e3a4
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-09 10:44:16 -0700

    3-print_alphabets.c style correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main - print out the alphabet in lowercase first, then uppercase, followed by a newline
+ * main - print out the alphabet in lowercase and  uppercase
  *
  * Return: 0
  */
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
