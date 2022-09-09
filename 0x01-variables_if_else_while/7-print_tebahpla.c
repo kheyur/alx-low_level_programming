9258b5a23ae35fe782950442d9d1f714bf44f5b4
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-09 13:23:50 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print the alphabet in reverse
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
