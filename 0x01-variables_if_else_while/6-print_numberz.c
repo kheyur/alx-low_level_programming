9cdd96886fb02a80b8372aa1948f953698d0fa6b
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-09 11:23:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print base 10 digits from 0 using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i%10);
+	}
+	putchar('\n');
+	return (0);
+}
