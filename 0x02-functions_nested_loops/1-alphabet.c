31a7235eeae10ec50d9b6deb73b6e348357c51f2
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-13 06:35:05 -0700

    1-alphabet.c commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: (0)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= z; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
