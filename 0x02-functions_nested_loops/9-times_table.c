1e7d6c4f33a9a856e6628b6ea916d5447756d6a7
kheyur <43371735+kheyur@users.noreply.github.com>
2022-09-15 01:48:12 +0200

    9-times_table correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,5 +3,27 @@
  */
 void times_table(void)
 {
+	int i, j, sum;
 
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
+		{
+			sum = i * j;
+			_putchar(44);
+			_putchar(32);
+			if (sum <= 9)
+			{
+				_putchar(32);
+				_putchar(sum + 48);
+			}
+			else
+			{
+				_putchar((sum / 10) + 48);
+				_putchar((sum % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
 }
