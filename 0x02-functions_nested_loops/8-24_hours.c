71c283c49f8a7ed3bb674fad3c2d8c7d7310ea3f
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-14 15:54:47 -0700

    8-24_hours.c correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,27 @@
-#inlcude 
+#include "main.h"
+/**
+ * jack_bauer - print time for a 24 hour cycle
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+		for (j = 48; j <53 ; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
