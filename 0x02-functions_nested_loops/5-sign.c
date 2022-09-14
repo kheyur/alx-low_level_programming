bf3b82ed5adf66cab938d8e0ec899c4af1cac6cc
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-14 04:38:58 -0700

    5-sign.c commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print-sign - print if number is + or -
+ * @n: pass through the number
+ *
+ * Return: 1 if > 0, 0 if = 0, -1 if < 0
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1)
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
