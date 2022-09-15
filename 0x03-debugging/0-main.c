4796d3e3e9e7229344811648d02bcba9f33ab365
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-15 03:59:34 -0700

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,11 @@
 /**
  * main - executes the positive/negative function
  *
- * Return: 0;
+ * Return: return 0
  */
 int main(void)
 {
-	int i;
-	
-	i = 0;
-	positive_or_negative(i);
+	positive_or_negative(0);
 
 	return (0);
 }
