a41e30bc565be09e9db2afa2536090db1f25f485
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-15 04:46:15 -0700

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
  * main - checks if number is + or -
- *
- * Return: return 0
  */
 int main(void)
 {
@@ -10,5 +8,4 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-	return (0);
 }
