aeade2f0d280a5def32af07875bbcda75da295d6
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-19 15:44:32 -0700

    4-print_rev.c commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: string to reverse
+ */
+void print_rev(char *s)
+{
+	int count;
+
+	count = 0;
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			break;
+		}
+		count++
+	}
+	for (count--; count >= 0; count--)
+	{
+		putchar(s[count]);
+	}
+	putchar('\n');
+}
