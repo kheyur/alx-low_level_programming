29c1326317d2bdf7f93f4b3d76a37aa2ff887244
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-19 13:51:29 -0700

    0-reset_to_98.c commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * reset_to_98 - set a variable to 98
+ * @n: int variable passed into method
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
