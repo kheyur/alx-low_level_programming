8f58bc68266b5c85fd4fd414060605f178677ad8
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-19 16:17:14 -0700

    5-rev_string commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,2 +1,30 @@
 #include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s - string to pass
+ */
+void rev_string(char *s)
+{
+	int count, i, j;
+	char tempStr1, tempStr2;
 
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			break;
+		}
+		count;;
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
+}
