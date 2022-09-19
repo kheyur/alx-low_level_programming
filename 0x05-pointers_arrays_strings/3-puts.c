8dabb51dbd4f906eefcd340edfe455342a7fa05a
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-19 15:19:41 -0700

    correciton

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,5 +7,16 @@
  */
 void _puts(char *str)
 {
-	putchar(*str + '\n');
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
 }
