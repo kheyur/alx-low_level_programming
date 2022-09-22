63c0d23ab9a71520286ed02e2af2650a29b9a474
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-22 15:53:52 -0700

    ccorection

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (*src && n--)
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[i] = src[i];
 	}
-	*dest = '\0';
+	for ( ; i < n; i++)
+	{
+			dest[i] = '\0';
+	}
 	return (dest);
 }
