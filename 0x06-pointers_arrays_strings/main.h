d729564c555d59234c0632296adb90b9882b04cd
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-22 14:49:56 -0700

    main.h correction

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+
+#endif
