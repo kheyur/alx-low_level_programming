787368d57dca9c84995711734c17c7988312c490
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-22 15:28:29 -0700

    1-strncat.c commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * strncat - concatenate 2 strings with n bytes and no null value
+ * @dest: string to be added to
+ * @src: string to add to dest
+ * @n: number of bytes to use from src
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+		{
+			break;
+		}
+		i++;
+		j++;
+	}
+	return (dest);
+}
