83ccc248924939e5a236f38af978a9eda2ed2b22
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-26 14:20:49 -0700

    2-strchr.c commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to look for
+ * Return: pointer to first occurence of c or NULL if none
+ */
+char *_strchar(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
