ecf1b27c8c3db36b1406050e91155f4a506dfac3
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-26 15:50:59 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - search string for any of a set of bytes
+ * @s: string
+ * @accept: set of characters
+ * Return: pointer to s that matches accept or null if none
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, k;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ('\0');
+}
