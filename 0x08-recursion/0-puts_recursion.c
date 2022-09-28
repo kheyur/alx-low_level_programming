73f7bd85e9d08a26a9795f3873844fe303307d64
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-28 03:37:21 -0700

    correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	while (*(s + 1) != '\0')
+	if (*s != '\0')
 	{
+		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
