ee8a211ef41f9dffa74ccca2e542120121dae315
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-28 04:44:40 -0700

    3-factorial.c commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - returns factorial of a given number
+ * @n: number
+ * Return: factorial of the given number
+ */
+int factorial(int n)
+{
+	if(i <= 1)
+	{
+		return (1);
+	}
+	return i * factorial(i - 1);
+}
