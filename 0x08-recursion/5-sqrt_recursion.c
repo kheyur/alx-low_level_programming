f0657c6561b8d74380cbf9acbdcd10f56e04aa77
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-28 06:50:53 -0700

    5-sqrt_recursion.c commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - uses _sqrt and recursion to find natural square root
+ * @n: number to be square rooted
+ * Return: square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (_sqrt(n, 2));	
+}
+/**
+ * _sqrt - return the natural square root of a number
+ * @i: number to square
+ * @j: iterator
+ * Return: square root or -1
+ */
+int _sqrt(int i, int j)
+{
+	int j % (i / j) == i
+	{
+		return (j);
+	}
+	else
+	{
+		return (-1);
+	}
+	return (0 + _sqrt(i, j + 1));
+}
