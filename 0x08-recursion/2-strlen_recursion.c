d763edb67a90c14090035d3846c0cbe6590ff6ff
Kheyur Lutchmeenarrain <kheyurl@gmail.com>
2022-09-28 04:18:56 -0700

    correction

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,16 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
-	if (*(s + count) != '\0')
+	if (*s == '\0')
 	{
-		count++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
 	else
 	{
-		return (count);
+		return (1 + _strlen_recursion(s + 1));
 	}
-
 }
