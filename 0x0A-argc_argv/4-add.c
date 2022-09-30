ea3ddb9ee07f193aa990b214ad566403a38c4d09
kheyur <kheyurl@gmail.com>
2022-09-30 13:34:17 -0700

    4-add.c commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - add positive numbers
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
