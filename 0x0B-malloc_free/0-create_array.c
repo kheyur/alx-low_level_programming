bf8e1ff1a3b59477112f2a5118f655ad22625e28
kheyur <kheyurl@gmail.com>
2022-10-04 05:20:14 -0700

    correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * @c: char to initialize array
  * Return: null if fail or size = 0; pointer to array if successful
  */
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	c = malloc(sizoef(unsigned int) * size);
 	if (c == NULL)
