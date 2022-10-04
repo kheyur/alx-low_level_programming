dcfc3515a8a391cc30b0d585e6e16f7d0d4ab8a8
kheyur <kheyurl@gmail.com>
2022-10-04 05:14:03 -0700

    0-create_array.c & README.md commit

---
+++ 0x0B-malloc-free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * create_array - create an array of chars and initialize it with a specific char
+ * @size: size of the array
+ * @c: char to initialize array
+ * Return: null if fail or size = 0; pointer to array if successful
+ */
+char *create_array(unsigned int size, char c);
+{
+	c = malloc(sizoef(unsigned int) * size);
+	if (c == NULL)
+	{
+		printf("Failed to allocate $d bytes\n", sizeof(unsigned int) * size);
+		return (NULL);
+	}
+	else
+	{
+		return (c);
+	}
+}
---
+++ 0x0B-malloc-free/README.md
@@ -0,0 +1,1 @@
+How malloc and free are used to allocate memory spaces dynamically for use as well as how to release the memory held in it using free
