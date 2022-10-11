ae1dc8c14fe4e651c7c5776c39159b965aca3a06
kheyur <43371735+kheyur@users.noreply.github.com>
2022-10-12 00:17:34 +0200

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
+/**
+ * dog_t - struct dog
+ */
+typedef struct dog dog_t;
+
 /**
  * struct dog - struct to create type of dog
  * @name: dog's name
@@ -17,6 +23,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
