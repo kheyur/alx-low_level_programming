d720b073ee070dc65bd81669303f07a21cb9494f
kheyur <kheyurl@gmail.com>
2022-10-11 15:12:15 -0700

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- * dog_t - struct
- */
-typedef struct dog dog_t;
-
-/**
  * struct dog - struct to create type of dog
  * @name: dog's name
  * @age: dog's age
@@ -22,7 +17,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
