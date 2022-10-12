52130868dcf0aaf39b3d197a1071ef5618855331
kheyur <43371735+kheyur@users.noreply.github.com>
2022-10-12 02:32:47 +0200

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@
  * @owner: new dog's owner
  * Return: struct dog, NULL if failed
  */
-dog_t *new_dog(char *name, flaot age, char *owned)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
 	int i, new_Name, new_Owner;
@@ -19,18 +19,15 @@ dog_t *new_dog(char *name, flaot age, char *owned)
 	}
 
 	for (new_Name = 0; name[new_Name]; new_Name++)
-	{
 		;
-	}
+
 	for (new_Owner = 0; owner[new_Owner]; new_Owner++)
-	{
 		;
-	}
 
 	n_dog->name = malloc(new_Name + 1);
 	n_dog->owner = malloc(new_Owner + 1);
 
-	if (!(p_dog->name) || !(p_dog->owner))
+	if (!(n_dog->name) || !(n_dog->owner))
 	{
 		free(n_dog->owner);
 		free(n_dog->name);
@@ -38,18 +35,14 @@ dog_t *new_dog(char *name, flaot age, char *owned)
 		return (NULL);
 	}
 
-	for (i = -; i < new_Name; i++)
-	{
+	for (i = 0; i < new_Name; i++)
 		n_dog->name[i] = name[i];
-	}
 	n_dog->name[i] = '\0';
 
 	n_dog->age = age;
 
-	for (i = 0; i < owner; i++)
-	{
+	for (i = 0; i < new_Owner; i++)
 		n_dog->owner[i] = owner[i];
-	}
 	n_dog->owner[i] = '\0';
 
 	return (n_dog);
