a07c3cbadbafeabf00542e745b3725a839f7a9ca
kheyur <kheyurl@gmail.com>
2022-10-12 16:24:42 -0700

    2-int_index.c commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an interger
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: pointer to function that compares values
+ * Return: first index that does not return 0, -1 if no matches
+ * or size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (-1);
+		}
+
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
