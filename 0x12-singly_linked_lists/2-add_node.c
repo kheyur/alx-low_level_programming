24441e761c165dc8b363e731b99767f436bf7c85
kheyur <kheyurl@gmail.com>
2022-10-20 05:49:55 -0700

    2-add_node.c commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * add_node - add new node to beginning of list
+ * @head: first element in list
+ * @str: string to duplicate
+ * Return: address of new element, NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t newLen;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	for (newLen = o; str[newLen]; newLen++)
+		;
+
+	new->len = newLen;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}	
