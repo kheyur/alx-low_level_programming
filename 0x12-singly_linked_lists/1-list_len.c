b5d1d8624e0c5cfdcf3f1ea9c93d6ec55283aba4
kheyur <kheyurl@gmail.com>
2022-10-20 05:33:27 -0700

    documentation correction

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * list_len returns number of elements in a linked list_t list
+ * list_len - returns number of elements in a linked list_t list
  * @h: singly linked list
  * Return: number of nodes
  */
