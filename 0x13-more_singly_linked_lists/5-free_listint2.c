f9d147774ae2a48d85a4e13e6648062f094fdb23
kheyur <kheyurl@gmail.com>
2022-10-24 14:45:36 -0700

    correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,7 +14,7 @@ void free_listint2(listint_t **head)
 		current = *head;
 		while ((temp = current) != NULL)
 		{
-			current = current-> next;
+			current = current->next;
 			free(temp);
 		}
 		*head = NULL;
