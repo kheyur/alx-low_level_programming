2dd5cabee4a469560a3797a7671fba611400e4ab
kheyur <43371735+kheyur@users.noreply.github.com>
2022-10-25 00:03:55 +0200

    correction

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -7,14 +7,16 @@
 int pop_listint(listint_t **head)
 {
 	int headn;
-	listint_t = *h;
-	listint_t = *current;
+	listint_t *h;
+	listint_t *current;
 
 	if (*head == NULL)
 		return (0);
 
 	current = *head;
+
 	headn = current->n;
+
 	h = current->next;
 
 	free(current);
