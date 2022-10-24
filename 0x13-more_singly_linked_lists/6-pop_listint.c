21f026c8caa22083e9fbfc966c942f59bf960198
kheyur <kheyurl@gmail.com>
2022-10-24 14:54:14 -0700

    6-pop_listint.c commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * dealetes head node of a linked list
+ * @head: beginning of linked list
+ * Return: head nodes data or 0 if list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	int headn;
+	listint_t = *h;
+	listint_t = *current;
+
+	if (*head == NULL)
+		return (0);
+
+	current = *head;
+	headn = current->n;
+	h = current->next;
+	free(current);
+	*head = h;
+	return (headn);
+}
