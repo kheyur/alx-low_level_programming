dde7280088c737a5da8e04c9f16b373a6e2fb6cd
kheyur <kheyurl@gmail.com>
2022-10-24 14:22:00 -0700

    4-free_listint.c commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - freees a listint_t list
+ * @head: beginning of list
+ * Return: none
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
