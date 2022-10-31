d9e2c1fece0f27926c22f17217a506e7a15232e2
kheyur <kheyurl@gmail.com>
2022-10-31 13:15:48 -0700

    2-append_text_to_file.c commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * appent_text_to_file - appends text at the end of a file
+ * @filename: name of file
+ * @text_content: text to append
+ * Return: 1 on success, -1 if fail or filename is NULL
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int n;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (n = 0; text_content[n]; n++)
+			;
+
+		rwr = write(fd, text_content, n);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
