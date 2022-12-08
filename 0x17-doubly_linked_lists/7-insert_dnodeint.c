e68bd6e48109eaffe8dd6d7d5f55b047de46a993
kheyur <43371735+kheyur@users.noreply.github.com>
2022-12-08 17:06:59 +0200

    documentation correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - inserts a new node at 
+ * insert_dnodeint_at_index - inserts a new node at
  * a given position
  *
  * @h: head of the list
